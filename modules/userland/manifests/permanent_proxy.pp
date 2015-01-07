e34c48585c3e86e6b2bac8532d07573803868208
slashme <pierre-arthur.mathieu@hp.com>
2015-01-07 23:03:04 +0000

    permanent proxy

--- modules/userland/manifests/permanent_proxy.pp
+++ modules/userland/manifests/permanent_proxy.pp
@@ -1,5 +1,5 @@
 class userland::permanent_proxy {
-    file { '/tmp/test':
+    file { '/etc/profile.d/proxy.sh':
         ensure  => file,
         content => template('userland/permanent_proxy.erb'),
         mode    => '755',
