9df1c5fa9aa40f7c4a4a5bea9e0b72056e599b80
slashme <pierre-arthur.mathieu@hp.com>
2015-01-07 23:59:08 +0000

    Root vim home

--- modules/userland/manifests/vim.pp
+++ modules/userland/manifests/vim.pp
@@ -68,7 +68,7 @@ class userland::vim(
             command     => "/usr/bin/vim -u /root/.vim/.vimrcInsVundle +PluginInstall +qall",
             require     => File["/root/.vim"],
             onlyif      => "/usr/bin/ls /root/.vim/bundle | wc -l | grep 1",
-            environment => ["http_proxy=$userland::installer::httpProxy","https_proxy=$userland::installer::httpsProxy"],
+            environment => ["http_proxy=$userland::installer::httpProxy","https_proxy=$userland::installer::httpsProxy","HOME=/root"],
         }
     }
 }
